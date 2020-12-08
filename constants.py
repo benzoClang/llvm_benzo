2bdac494101b0e8f5720b6849e8559e3da95d9e9
Yi Kong <yikong@google.com>
2020-12-09 02:03:31 +0800

    Build Stage1 with AArch64 support
    ... so that we can build stage-2 clang in AArch64.
    
    Test: presubmit
    Bug: 173856309
    Change-Id: I6ce513c932b40b2d7218e9e2c55a6af6ff6b22d4
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- constants.py
+++ constants.py
@@ -24,7 +24,7 @@ CLANG_PREBUILT_VERSION: str = 'clang-bootstrap'
 NDK_VERSION: str = 'r20'
 
 # Targets for host.
-HOST_TARGETS: Set[str] = set(['X86'])
+HOST_TARGETS: Set[str] = set(['X86', 'AArch64'])
 
 # Targets for Android.
 ANDROID_TARGETS: Set[str] = set(['AArch64', 'ARM', 'BPF', 'X86'])
