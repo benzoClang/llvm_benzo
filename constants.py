aosp-riscv/toolchain_llvm_android

Update toolchain for stage1 to clang-r433403
Test: presubmit
Change-Id: I7b6ee1957833bb204cf33a4e03a58d469b298875

constants.py
@@ -21,7 +21,7 @@ from typing import Set
 MAC_MIN_VERSION: str = '10.14'
 
 # This is the baseline stable version of Clang to start our stage-1 build.
-CLANG_PREBUILT_VERSION: str = 'clang-r428724'
+CLANG_PREBUILT_VERSION: str = 'clang-r433404'
 
 # This is the ndk version used to build runtimes.
 NDK_VERSION: str = 'r23'

benzoClang/llvm_benzo

Signed-off-by: Jason Edson <jaysonedson@gmail.com>

constants.py
@@ -18,7 +18,7 @@
 from typing import Set
 
 # This is the baseline stable version of Clang to start our stage-1 build.
-CLANG_PREBUILT_VERSION: str = 'clang-r416183b'
+CLANG_PREBUILT_VERSION: str = 'clang-r433403'
 
 # This is the ndk version used to build runtimes.
 NDK_VERSION: str = 'r23'