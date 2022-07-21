8ce0a71f36ad648f82a7c8623be33c3643a35d3a
Jason Edson <jaysonedson@gmail.com>
2022-07-21 07:44:52 -0700

    Switch android-ndk version to r25
    Change-Id: I2a07394b93e3314546d99aaf87ac2086f4ed0653
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- constants.py
+++ constants.py
@@ -21,7 +21,7 @@ from typing import Set
 CLANG_PREBUILT_VERSION: str = 'clang-r458507'
 
 # This is the ndk version used to build runtimes.
-NDK_VERSION: str = 'r23'
+NDK_VERSION: str = 'r25'
 
 # Targets for host.
 HOST_TARGETS: Set[str] = set(['X86', 'AArch64'])
